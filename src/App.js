b42271017fd7b831602e322db3b406c823bcae0e
Benjamin Spiegel <ben@neap.space>
2022-02-14 23:23:29 -0600

    Use bootstrap-icons instead of unicode glyphs

--- src/App.js
+++ src/App.js
@@ -383,12 +383,42 @@ class CatalogTableHeaderRow extends React.Component {
         return (
             <tr key="0">
                 {this.props.columns.map((column, index) => {
-                    let sortIndicator = ''
+                    let sortIndicator
                     if (this.props.sortColumn === column.key) {
                         if (this.props.sortAscending) {
-                            sortIndicator = ' ⏶'
+                            sortIndicator = (
+                                // From bootstrap-icons: caret-up-fill
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    class="bi bi-chevron-down"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                                    />
+                                </svg>
+                            )
                         } else {
-                            sortIndicator = ' ⏷'
+                            sortIndicator = (
+                                // From bootstrap-icons: caret-down-fill
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="16"
+                                    height="16"
+                                    fill="currentColor"
+                                    class="bi bi-chevron-up"
+                                    viewBox="0 0 16 16"
+                                >
+                                    <path
+                                        fill-rule="evenodd"
+                                        d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+                                    />
+                                </svg>
+                            )
                         }
                     }
 
@@ -397,8 +427,7 @@ class CatalogTableHeaderRow extends React.Component {
                             key={index}
                             onClick={() => this.props.callback(column.key)}
                         >
-                            {column.label}
-                            {sortIndicator}
+                            {column.label} {sortIndicator}
                         </th>
                     )
                 })}
