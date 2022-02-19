0a9bca175c564092b54b1485350c18a58dbcdf1b
Benjamin Spiegel <ben@neap.space>
2022-02-18 23:12:34 -0600

    Fix sort indicator direction

--- src/App.js
+++ src/App.js
@@ -394,12 +394,12 @@ class CatalogTableHeaderRow extends React.Component {
                                     width="16"
                                     height="16"
                                     fill="currentColor"
-                                    className="bi bi-chevron-down"
+                                    className="bi bi-chevron-up"
                                     viewBox="0 0 16 16"
                                 >
                                     <path
                                         fillRule="evenodd"
-                                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
+                                        d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
                                     />
                                 </svg>
                             )
@@ -411,12 +411,12 @@ class CatalogTableHeaderRow extends React.Component {
                                     width="16"
                                     height="16"
                                     fill="currentColor"
-                                    className="bi bi-chevron-up"
+                                    className="bi bi-chevron-down"
                                     viewBox="0 0 16 16"
                                 >
                                     <path
                                         fillRule="evenodd"
-                                        d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"
+                                        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
                                     />
                                 </svg>
                             )
